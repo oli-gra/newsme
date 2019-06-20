7f6b61b7e60bdfa38f825839680bda7ecb0eb44f
oli-gra <oliver.grant@gmail.com>
2019-06-20 09:35:28 +0100

    Updates

---
+++ precache-manifest.b28c5d50ae645818e5e21af83ff290d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c988ea0392dfb7203cbe977ed636ddd1",
+    "url": "/oli-gra/newsme/index.html"
+  },
+  {
+    "revision": "caca63b575fad30cff5b",
+    "url": "/oli-gra/newsme/static/css/main.618a1940.chunk.css"
+  },
+  {
+    "revision": "cacecd47398e41bad5aa",
+    "url": "/oli-gra/newsme/static/js/2.5b10c1cb.chunk.js"
+  },
+  {
+    "revision": "caca63b575fad30cff5b",
+    "url": "/oli-gra/newsme/static/js/main.3be610d6.chunk.js"
+  },
+  {
+    "revision": "d6993f5d70a0f1c4579a",
+    "url": "/oli-gra/newsme/static/js/runtime~main.ce17afee.js"
+  }
+]);
